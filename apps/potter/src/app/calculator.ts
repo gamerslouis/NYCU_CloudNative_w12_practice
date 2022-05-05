bcc3d75bb10ccca7600e2747313109525e25457d
gamerslouis <gamerslouis@gmail.com>
2022-05-06 01:09:13 +0800

    Handle Edge cases

--- apps/potter/src/app/calculator.ts
+++ apps/potter/src/app/calculator.ts
@@ -8,27 +8,53 @@ export class Calculator {
   cal(books: Books): number {
     let bookqueue = books.filter((v) => v !== 0);
     bookqueue.sort();
-
-    let totalPrice = 0;
+    let pricequeue: Array<number> = [];
 
     while (bookqueue.length > 0) {
       if (bookqueue[0] === 0) {
-        bookqueue = bookqueue.splice(1);
+        bookqueue = bookqueue.splice(0);
         continue;
       }
-      totalPrice += this.simpleDiscountTotal(bookqueue[0], bookqueue.length);
+      pricequeue = pricequeue.concat(
+        this.makeFilledArray(bookqueue[0], bookqueue.length)
+      );
       const toReduce = bookqueue[0];
       bookqueue = bookqueue.map((val) => val - toReduce);
       bookqueue = bookqueue.filter((v) => v !== 0);
     }
+
+    pricequeue = this.handleSpecialCaseThat44isBetterThen53(pricequeue)
+
+    let totalPrice = 0;
+    for (const val of pricequeue) {
+      totalPrice += this.simpleDiscount(val);
+    }
+
     return totalPrice;
   }
 
-  simpleDiscountTotal(numPerBook: number, numBookCategory: number) {
-    return (
-      BASE_PRICE *
-      DiscountRate[numBookCategory] *
-      (numPerBook * numBookCategory)
-    );
+  handleSpecialCaseThat44isBetterThen53(pricequeue: Array<number>) {
+    while (pricequeue.indexOf(5) !== -1 && pricequeue.indexOf(3) !== -1) {
+      pricequeue = this.removeItemOnce(pricequeue, 5);
+      pricequeue = this.removeItemOnce(pricequeue, 3);
+      pricequeue = pricequeue.concat([4, 4]);
+    }
+    return pricequeue
+  }
+
+  simpleDiscount(numBookCategory: number) {
+    return BASE_PRICE * DiscountRate[numBookCategory] * numBookCategory;
+  }
+
+  makeFilledArray(length: number, value: number) {
+    return Array(length).fill(value);
+  }
+
+  removeItemOnce(arr: Array<number>, value: number) {
+    const index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
   }
 }
